8fac352017dd927ddab005506cecb05fed586579
Mohmed Khaled <138899914+Mohamedkhaled687@users.noreply.github.com>
2024-07-16 12:53:18 +0300

    Two pointers

---
+++ Leetcode/Longest Consecutive Sequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int longestStreak = 0;
+
+        for (const int& num : numSet) {
+            if (numSet.find(num - 1) == numSet.end()) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.find(currentNum + 1) != numSet.end()) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+};
